328eb48190e7763edca84d1f8329fd6788458aa0
otsembo <okumu.otsembo@gmail.com>
2024-09-02 05:59:22 +0300

    Add docker configuration

--- server/src/main/kotlin/com/otsembo/portfolio/infrastructure/config/DBConfigs.kt
+++ server/src/main/kotlin/com/otsembo/portfolio/infrastructure/config/DBConfigs.kt
@@ -4,6 +4,8 @@ import com.otsembo.portfolio.application.adapters.data.DBEntities
 import com.otsembo.portfolio.infrastructure.db.IDBConnection
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
+import io.ktor.server.application.Application
+import org.flywaydb.core.Flyway
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
@@ -19,34 +21,73 @@ object DBConfigs {
 
     // Database Engines - START
 
-    private val h2 =
-        DBConnection(
-            dataSource =
-                HikariDataSource(
-                    baseHikariConfig.apply {
-                        jdbcUrl = "jdbc:h2:mem:test"
-                        driverClassName = "org.h2.Driver"
-                    },
-                ),
-        )
-
-    // Database Engines - END
-
-    private fun getConnection(engine: String = "h2") =
-        when (engine.lowercase()) {
-            "h2" -> h2
+    private fun Application.engineDefinitions(engine: String): Map<String, String> =
+        when (engine) {
+            "h2" ->
+                mapOf(
+                    "jdbcDriver" to environment.config.property("db.h2.jdbcDriver").getString(),
+                    "connURL" to environment.config.property("db.h2.connURL").getString(),
+                )
+            "postgresql" ->
+                mapOf(
+                    "jdbcDriver" to
+                        environment.config.property("db.postgresql.jdbcDriver").getString(),
+                    "connURL" to environment.config.property("db.postgresql.connURL").getString(),
+                )
             else -> throw IllegalArgumentException("Invalid database engine: $engine")
         }
 
-    fun initDB(engine: String = "h2") {
-        getConnection(engine)
+    private val Application.h2: DBConnection
+        get() =
+            DBConnection(
+                dataSource =
+                    HikariDataSource(
+                        baseHikariConfig.apply {
+                            jdbcUrl = engineDefinitions("h2")["connURL"]
+                            driverClassName = engineDefinitions("h2")["jdbcDriver"]
+                        },
+                    ),
+            )
+
+    private val Application.postgresql: DBConnection
+        get() =
+            DBConnection(
+                dataSource =
+                    HikariDataSource(
+                        baseHikariConfig.apply {
+                            jdbcUrl = engineDefinitions("postgresql")["connURL"]
+                            driverClassName = engineDefinitions("postgresql")["jdbcDriver"]
+                        },
+                    ),
+            )
+
+    // Database Engines - END
+
+    private fun Application.getConnection(engine: String = "h2") =
+        when (engine.lowercase()) {
+            "h2" -> h2
+            "postgresql" -> postgresql
+            else -> throw IllegalArgumentException("Invalid database engine: $engine")
+        }
+
+    fun Application.dbModule(engine: String = "h2") {
+        val connection = getConnection(engine)
         transaction {
             SchemaUtils.create(*DBEntities.schemas)
         }
+        setUpMigrations(connection.dataSource)
+    }
+
+    private fun setUpMigrations(dataSource: DataSource) {
+        Flyway
+            .configure()
+            .dataSource(dataSource)
+            .load()
+            .migrate()
     }
 
     class DBConnection(
-        dataSource: DataSource,
+        val dataSource: DataSource,
     ) : IDBConnection {
         override var db: Database? = null
 
