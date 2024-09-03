0a2cb785855a2a4583e8d19d951bd997d11b0a1b
otsembo <okumu.otsembo@gmail.com>
2024-09-03 22:12:29 +0300

    feat: add app buttons

--- composeApp/src/commonMain/kotlin/com/otsembo/portfolio/App.kt
+++ composeApp/src/commonMain/kotlin/com/otsembo/portfolio/App.kt
@@ -2,40 +2,56 @@
 
 package com.otsembo.portfolio
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
+import androidx.compose.ui.unit.dp
+import com.otsembo.portfolio.presentation.components.AppButton
+import com.otsembo.portfolio.presentation.components.AppButtonType
+import com.otsembo.portfolio.presentation.theme.AppTheme
 import org.jetbrains.compose.ui.tooling.preview.Preview
-import otsembo_portfolio.composeapp.generated.resources.Res
-import otsembo_portfolio.composeapp.generated.resources.compose_multiplatform
 
 @Suppress("ktlint:standard:function-naming")
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
+    AppTheme {
+        Scaffold(
+            modifier =
+                Modifier
+                    .fillMaxSize(),
+        ) {
+            Column(
+                modifier =
+                    Modifier
+                        .padding(
+                            horizontal = 16.dp,
+                        ).padding(top = 8.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+            ) {
+                AppButton(
+                    modifier = Modifier.height(ButtonDefaults.MinHeight),
+                    onClick = {},
+                    content = {
+                        Text("Button")
+                    },
+                )
+
+                AppButton(
+                    modifier = Modifier.height(ButtonDefaults.MinHeight),
+                    onClick = {},
+                    content = {
+                        Text("Button")
+                    },
+                    type = AppButtonType.SECONDARY,
+                )
             }
         }
     }
--- composeApp/src/commonMain/kotlin/com/otsembo/portfolio/presentation/components/Buttons.kt
+++ composeApp/src/commonMain/kotlin/com/otsembo/portfolio/presentation/components/Buttons.kt
@@ -1,2 +1,68 @@
 package com.otsembo.portfolio.presentation.components
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun AppButton(
+    modifier: Modifier = Modifier,
+    type: AppButtonType = AppButtonType.PRIMARY,
+    onClick: () -> Unit,
+    content: @Composable RowScope.() -> Unit,
+) {
+    val (btnColors, border) =
+        when (type) {
+            AppButtonType.PRIMARY -> (
+                ButtonDefaults
+                    .outlinedButtonColors()
+                    .copy(
+                        containerColor = Color.Transparent,
+                        disabledContainerColor = Color.Transparent,
+                        contentColor = MaterialTheme.colorScheme.onSurface,
+                    )
+                    to
+                    BorderStroke(
+                        width = 2.dp,
+                        color = MaterialTheme.colorScheme.primary,
+                    )
+            )
+            AppButtonType.SECONDARY -> (
+                ButtonDefaults
+                    .outlinedButtonColors()
+                    .copy(
+                        containerColor = Color.Transparent,
+                        disabledContainerColor = Color.Transparent,
+                        contentColor = MaterialTheme.colorScheme.onSurface,
+                    )
+                    to
+                    BorderStroke(
+                        width = 2.dp,
+                        color = MaterialTheme.colorScheme.onSurface,
+                    )
+            )
+            AppButtonType.TERTIARY -> TODO()
+        }
+
+    OutlinedButton(
+        modifier = modifier,
+        onClick = onClick,
+        content = content,
+        shape = RoundedCornerShape(percent = 50),
+        colors = btnColors,
+        border = border,
+    )
+}
+
+enum class AppButtonType {
+    PRIMARY,
+    SECONDARY,
+    TERTIARY,
+}
